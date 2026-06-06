12bca806233106bbb7afd528f5cc836e8cab01b5
DaBloat <kurtrusselvillamor1206@gmail.com>
2026-06-06 19:55:55 +0800

    added: gtk and qt themes + zen css

--- hypr/modules/environment.lua
+++ hypr/modules/environment.lua
@@ -17,3 +17,15 @@ hl.env("LIBVA_DRIVER_NAME", "radeonsi")
 -- NVIDIA
 hl.env("GBM_BACKEND", "nvidia-drm")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+
+-- QT
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+
+
+hl.env("MOZ_ENABLE_WAYLAND", "1")
+hl.env("CLUTTER_BACKEND", "wayland")
+
+
